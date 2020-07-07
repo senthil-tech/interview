401bbe46f46594eba3f293943371d429fc7a6f4c
senthil-tech <67146572+senthil-tech@users.noreply.github.com>
2020-07-08 02:59:35 +0800

    Update UserRepository.java

--- src/main/java/com/interview/template/dao/UserRepository.java
+++ src/main/java/com/interview/template/dao/UserRepository.java
@@ -1,9 +1,15 @@
 package com.interview.template.dao;
 
 import com.interview.template.model.UserEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface UserRepository extends JpaRepository<UserEntity, Long> {
+    
+    @Query("SELECT u FROM UserEntity u WHERE u.username LIKE :username")
+    List<UserEntity> searchUserByName(@Param("username") String username);
 }
